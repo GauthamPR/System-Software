8ed277a6dd2b7e9146220eab0d0113e06c8505d0
Gautham P R <gauthampr18@gmail.com>
2020-11-11 09:51:45 +0530

    Standard Version

--- src/Exp_7.c
+++ src/Exp_7.c
@@ -5,35 +5,24 @@
 const int maxChopstick = 5;
 int chopsticks[5];
 
-int idPrompt();
-void startEat(int);
-void stopEat(int);
+void eat();
 void wait(int *);
 void signal(int *);
-void printChopsticks();
-void first();
+void initChopsticks();
+int idPrompt();
 
 void main(){
     int option;
-    first();
-    printf(" %d", chopsticks[0]);
+    initChopsticks();
     while(1){
-        printf("\nEnter Choice:     1.Start Eating     2.Stop Eating      3.Print Chopsticks       4.Exit :  ");
+        printf("\n\nEnter Choice:     1.Eat     2.Exit :  ");
         scanf("%d", &option);
         switch (option){
             case 1:
-                startEat(idPrompt());
-                break;
-            
-            case 2:
-                stopEat(idPrompt());
+                eat(idPrompt());
                 break;
 
-            case 3:
-                printChopsticks();
-                break;
-            
-            case 4:
+            case 2:
                 return ;
                 break;
 
@@ -45,27 +34,28 @@ void main(){
 }
 int idPrompt(){
     int id;
-    printf("Enter philosophser Id: ");
+    printf("\tEnter Philosophser Id: ");
     scanf("%d", &id);
     return id;
 }
 
-void startEat(int id){
-    printf("Started Eating");
+void eat(int id){
     wait(&chopsticks[id]);
+    printf("\n\t\tPicked left Chopstick");
     wait(&chopsticks[(id+1)%maxChopstick]);
-}
+    printf("\n\t\tPicked right Chopstick");
 
-void stopEat(int id){
+    printf("\n\n\t\tEating......");
+    printf("\n\n\t\tStopped Eating");
+    
     signal(&chopsticks[id]);
-    signal(&chopsticks[(id+1)%maxChopstick]); 
+    printf("\n\n\t\tPut Down left Chopstick");
+    signal(&chopsticks[(id+1)%maxChopstick]);
+    printf("\n\t\tPut Down right Chopstick");
 }
 
 void wait(int *s){
-    if((*s)<=0){
-        printf("\nChopstick Not free");
-        return ;
-    }
+    while((*s)<=0);
     (*s)--;
 }
 
@@ -73,14 +63,7 @@ void signal(int *s){
     (*s)++;
 }
 
-void printChopsticks(){
-    printf("Buffer:");
-    for(int i=0; i<maxChopstick; i++){
-        printf(" %d", chopsticks[i]);
-    }
-    printf("\n");
-}
-void first(){
+void initChopsticks(){
     for(int i=0; i<maxChopstick; i++){
         chopsticks[i] = 1;
     }
