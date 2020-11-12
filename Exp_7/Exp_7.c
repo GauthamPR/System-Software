967ac51cefe0717edbbde638011e50a492961cdb
Gautham P R <gauthampr18@gmail.com>
2020-11-12 23:37:56 +0530

    Exp 7
    Maybe working

--- Exp_7/Exp_7.c
+++ Exp_7/Exp_7.c
@@ -3,45 +3,86 @@
 #include<string.h>
 
 int numberOfChopsticks, numberOfPhilosophers;
-int chopsticks[5];
+int chopsticks[10];
 
-struct Process{
-    int id;
-    struct Process * next;
-}ready;
-
-void wait(int *);
+int wait(int *);
 void signal(int *);
 void initChopsticks();
-void tryEat(int);
+int startEat(int);
+void stopEat(int);
+void printChops();
 
 void main(){
+    int finishedEating[10];
+    memset(finishedEating, 0, 10*sizeof(int));
+    int count = 0;
+    printf("%d", finishedEating[4]);
     printf("\n\nEnter Number Of Philosophers: ");
     scanf("%d", &numberOfPhilosophers);
+    numberOfChopsticks = numberOfPhilosophers;
     initChopsticks();
-    while(traverse != NULL){
-        
+    while(count < numberOfPhilosophers){
+        for(int i=0; i<numberOfPhilosophers; i++){
+            printChops();
+            if(finishedEating[i] < 2){
+                if(startEat(i) == 1){
+                    printf("\nPhilosopher %d is eating", i+1);
+                    finishedEating[i] = 1;
+                }else{
+                    printf("\nPhilosopher %d is thinking", i+1);
+                    if(finishedEating[i]==1){
+                        count++;
+                        finishedEating[i]=2;
+                    }
+                }
+            }else{
+                printf("\nPhilosopher %d is thinking2", i+1);
+            }
+            printChops();
+            printf("\n");
+        }
+        printf("\n");
     }
 }
 
-void tryEat(int i){
-    printf("\n\tPhilosopher %d has stopped Thinking", i+1);
-    
-    wait(&chopsticks[i]);
-    wait(&chopsticks[(i+1)%numberOfChopsticks]);
-    
-    printf("\n\tPhilosopher %d has started Eating", i+1);
+int startEat(int i){
+    int blocked = 0;
+    if(wait(&chopsticks[i]) == 0){
+        blocked++;
+    }
+    if(wait(&chopsticks[(i+1)%numberOfChopsticks]) == 0){
+        blocked += 2;
+    }
+
+    if(blocked == 0){
+        return 1;
+    }
+    if(blocked == 1){
+        signal(&chopsticks[(i+1)%numberOfChopsticks]);
+    }
+    if(blocked == 2){
+        signal(&chopsticks[i]);
+        return 0;
+    }
+    if(blocked == 3){
+        printf("\t\t\t\t\tHEHE");
+        signal(&chopsticks[i]);
+        signal(&chopsticks[(i+1)%numberOfChopsticks]);
+        return 0;
+    }
+}
+void stopEat(int i){
     printf("\n\tPhilosopher %d has stopped Eating", i+1);
     signal(&chopsticks[i]);
     signal(&chopsticks[(i+1)%numberOfChopsticks]);
 }
-
-void wait(int *s){
+int wait(int *s){
     if((*s)<=0){
-        struct Process * new = (struct Process *) malloc(sizeof(struct Process));
-
+        printf("BLOCKED");
+        return 0;
     }
     (*s)--;
+    return 1;
 }
 
 void signal(int *s){
@@ -49,8 +90,14 @@ void signal(int *s){
 }
 
 void initChopsticks(){
-
     for(int i=0; i<numberOfChopsticks; i++){
         chopsticks[i] = 1;
+    }
+    printChops();
+}
+void printChops(){
+    printf("\nChopStick: ");
+    for(int i=0; i<numberOfChopsticks; i++){
+        printf("%d",chopsticks[i]);
     }
 }
--- Exp_7/test.js
+++
@@ -1,3 +0,0 @@
-var readline = require('readline');
-var a = readline();
-console.log("a", a);
