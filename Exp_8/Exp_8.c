70aff323b83ef9349c2a85a6f42a64384eaff1da
Gautham P R <gauthampr18@gmail.com>
2020-11-16 22:48:20 +0530

    without file read and write

--- Exp_8/Exp_8.c
+++ Exp_8/Exp_8.c
@@ -19,15 +19,12 @@ void searchValue();
 void display();
 
 void initTables();
-void writeToFile();
-void readFromFile();
 
 void main(){
     int option;
     initTables();
-    readFromFile();
     while(1){
-        printf("\nEnter Option:\t1.Enter Values\t\t2.Write to file\t\t3.Search\t4.Display:\t5.Exit:\t");
+        printf("\nEnter Option:\t1.Enter Values\t\t2.Search\t3.Display:\t4.Exit:\t");
         scanf("%d", &option);
 
         switch (option)
@@ -36,15 +33,12 @@ void main(){
             enterValues();
             break;
         case 2:
-            writeToFile();
-            break;
-        case 3:
             searchValue();
             break;
-        case 4:
+        case 3:
             display();
             break;
-        case 5:
+        case 4:
             exit(1);
             break;
         default:
@@ -143,53 +137,4 @@ void initTables(){
         strcpy(table[i].label, initialLabelValue);
         table[i].next = NULL;
     }
-}
-
-void readFromFile(){
-    FILE *fptr;
-    fptr = fopen("sym.txt", "r");
-    if(fptr == NULL){
-        printf("\nERROR OPENING FILE FOR READ");
-        return ;
-    }
-    for(int i=0; i<maxRows; i++){
-        fread(&table[i], sizeof(struct elem), 1, fptr);
-        struct elem * traverse = &table[i];
-        struct elem * prev = &table[i];
-        while(traverse->next != NULL){
-            traverse = (struct traverse *) malloc(sizeof(struct elem));
-            fread(traverse, sizeof(struct elem), 1, fptr);
-            prev->next = traverse;
-            prev = prev->next;
-        }
-    }
-    if(fread != 0){
-        printf("\nFILE READ SUCCESSFULLY");
-    }else{
-        printf("\nERROR READING FILE");
-    }
-    fclose(fptr);
-}
-
-void writeToFile(){
-    FILE *fptr;
-    fptr = fopen("sym.txt", "w");
-    if(fptr == NULL){
-        printf("\nERROR OPENING FILE FOR WRITE\n");
-        return ;
-    }
-    for(int i=0; i<maxRows; i++){
-        fwrite(&table[i], sizeof(struct elem), 1, fptr);
-        struct elem * traverse = table[i].next;
-        while(traverse != NULL){
-            fwrite(traverse, sizeof(struct elem), 1, fptr);
-            traverse = traverse->next;
-        }
-    }
-    if(fwrite != 0){
-        printf("\n\tWROTE TO FILE SUCCESSFULLY\n");
-    }else{
-        printf("\nERROR WRITIING FILE\n");
-    }
-    fclose(fptr);
 }
